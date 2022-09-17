6fa4e80620dd8a26a20b6a02da7a712cf8a0e800
4ge44koyama <m220552a@gmail.com>
2022-09-17 14:11:28 +0900

    React.memoを使用して、再レンダリングの制御を試す

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from "react";
-import { ColoredMessage } from "./components/ColoredMessage";
+import { useState, memo } from "react";
+import { Child1 } from "./components/Child1";
+import { Child4 } from "./components/Child4";
 
-export const App = () => {
-  // Stateの定義
+export const App = memo(() => {
+  console.log('Appレンダリング');
+
   const [num, setNum] = useState(0);
 
-  // ボタンを押したときに走る処理
   const onClickButton = () => {
-    setNum((prev) => prev + 1);
+    setNum(num + 1);
   };
 
   return (
     <>
-      <h1 style={{ color: 'red' }}>こんにちは！</h1>
-      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
-      <ColoredMessage color="pink">元気です！</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
       <p>{num}</p>
+      <Child1 />
+      <Child4 />
     </>
   );
-};
+});
