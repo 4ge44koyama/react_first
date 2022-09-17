9309088e4ba3b6bdd056def22a1f8e25544b7af8
4ge44koyama <m220552a@gmail.com>
2022-09-17 22:24:28 +0900

    不要なpropsを削除

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export const App = () => {
       {/* 管理者フラグがtrueの時とそれ以外で文字を出し分け */}
       {isAdmin ? <span>管理者です</span> : <span>管理者以外です</span>}
       <button onClick={onClickSwitch}>切り替え</button>
-      <Card isAdmin={isAdmin} />
+      <Card />
     </div>
   );
 };
